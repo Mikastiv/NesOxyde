e7598f06406bcefd3458f18648ae70800e5f9fc4
mikastiv <mikastiv@outlook.com>
2021-05-01 16:32:07 -0400

    ram on mapper0

--- src/cartridge/mappers/mapper0.rs
+++ src/cartridge/mappers/mapper0.rs
@@ -3,16 +3,24 @@ use crate::cartridge::{MirrorMode, Rom};
 
 pub struct Mapper0 {
     rom: Rom,
+    ram: Vec<u8>,
 }
 
 impl Mapper0 {
     pub fn new(rom: Rom) -> Self {
-        Self { rom }
+        Self {
+            rom,
+            ram: vec![0; 0x2000],
+        }
     }
 }
 
 impl Mapper for Mapper0 {
     fn read_prg(&mut self, addr: u16) -> u8 {
+        if let 0x6000..=0x7FFF = addr {
+            return self.ram[(addr & 0x1FFF) as usize];
+        }
+
         let mask = if self.rom.header.prg_count() > 1 {
             0x7FFF
         } else {
@@ -21,7 +29,11 @@ impl Mapper for Mapper0 {
         self.rom.prg[(addr & mask) as usize]
     }
 
-    fn write_prg(&mut self, _addr: u16, _data: u8) {}
+    fn write_prg(&mut self, addr: u16, data: u8) {
+        if let 0x6000..=0x7FFF = addr {
+            self.ram[(addr & 0x1FFF) as usize] = data;
+        }
+    }
 
     fn read_chr(&mut self, addr: u16) -> u8 {
         self.rom.chr[addr as usize]
